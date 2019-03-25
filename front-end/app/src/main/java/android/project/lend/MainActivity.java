efa759a8bb54fbebbf324f4680171c5b394f565e
Erik Maresia <maresiaerik@gmail.com>
2019-03-25 13:23:58 +0200

    Added bottom navigation with fragments

--- front-end/app/src/main/java/android/project/lend/MainActivity.java
+++ front-end/app/src/main/java/android/project/lend/MainActivity.java
@@ -1,7 +1,11 @@
 package android.project.lend;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottom_nav = findViewById(R.id.bottom_navigation);
+
+        bottom_nav.setOnNavigationItemSelectedListener(nav_listener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ExploreFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener nav_listener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selected_fragment = null;
+                    switch (menuItem.getItemId()) {
+                        case R.id.navbar_explore:
+                            selected_fragment = new ExploreFragment();
+                            break;
+                        case R.id.navbar_lendz:
+                            selected_fragment = new LendzFragment();
+                            break;
+                        case R.id.navbar_home:
+                            selected_fragment = new HomeFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected_fragment).commit();
+                    return true;
+                }
+            };
 }
